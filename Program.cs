f8fd4ab142aa1f865bb7cead9661b1e27b2aa669
scott-mescudi <anish.chittu2007@gmail.com>
2025-10-29 02:49:15 +0100

    brr

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Http.Headers;
+using cache;
+
+class Program {
+    static void main() {
+        cache.TTLCache ch = new cache.TTLCache(new TimeSpan(seconds: 10));
+        ch.Method();
+    }
+}
---
+++ cache/cache.cs
@@ -0,0 +1,32 @@
+﻿namespace cache {
+    class CacheEntry {
+        public required string Value { get; set; }
+        
+        private readonly DateTime expiry;
+        public bool IsExpired => DateTime.Now > expiry;
+
+        public CacheEntry(string val, TimeSpan TTL) {
+            this.Value = val;
+            this.expiry = DateTime.Now + TTL;
+        }
+
+    }
+
+
+    public class TTLCache {
+        private TimeSpan CheckInterval;
+
+        public TTLCache(TimeSpan cleanInterval) {
+            this.CheckInterval = cleanInterval;
+        }
+
+        public void Method() {
+            Console.WriteLine("hello");
+        }
+
+    }
+}
+
+
+
+
